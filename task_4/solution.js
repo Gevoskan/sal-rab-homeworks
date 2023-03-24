3f8c35d1e3e974decac4b0e305215b67ccfdf96e
Kate Klim <tervoskan@bk.ru>
2023-03-24 13:39:46 +0300

    dz3

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,6 +1,20 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
+
+    function parseProducts(json) {
+        let data = JSON.parse(json);
+        return data.products;
+        }
+        
+        function renderProductsCards(json) {
+        clearProducts();
+        let products = parseProducts(json);
+        let length = products.length;
+        for (let i = 0; i < (length-1); i +=1) {
+        addProduct(products[i]);
+        }
+        
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
